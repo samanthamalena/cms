9dc94e2250cee6f39ee4974166038674c06d3ae4
samanthahancock <han13018@byui.edu>
2019-05-14 22:09:33 -0600

    added messages and documents

---
+++ src/app/documents/document.model.ts
@@ -0,0 +1,16 @@
+export class Document  {
+    public documentId: string;
+    public name: string;
+    public description: string;
+    public url: string;
+    public children: string;
+
+    constructor(documentId: string, name: string, description: string, url: string, children: string ){
+        this.documentId = documentId;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.children = children;
+
+    }
+}  
